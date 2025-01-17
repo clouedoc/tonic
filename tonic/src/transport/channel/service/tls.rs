ede993f8b3619c32cd4dd4c3fcd2743c5212af5e
Camille Louédoc-Eyriès <clouedoc@tutanota.com>
2025-01-17 23:09:45 +0700

    feat: support the SSLKEYLOGFILE environment variable for clients
    copied from here: https://github.com/hyperium/tonic/pull/1102/commits/a976330c6cc0c8cf495f199599c6d4e3aa043c98

--- tonic/src/transport/channel/service/tls.rs
+++ tonic/src/transport/channel/service/tls.rs
@@ -67,6 +67,7 @@ impl TlsConnector {
             None => builder.with_no_client_auth(),
         };
 
+        config.key_log = Arc::new(tokio_rustls::rustls::KeyLogFile::new());
         config.alpn_protocols.push(ALPN_H2.into());
         Ok(Self {
             config: Arc::new(config),
